7f3523ae9416d88524bc3b86f90ef1b76e5323e0
gauravstr07 <gauravstr05@gmail.com>
2026-05-09 14:36:05 +0530

    Constructor Injection

---
+++ src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -0,0 +1,32 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	private Coach myCoach;
+	
+	@Autowired
+	public DemoController(Coach theCoach) {
+		myCoach = theCoach;
+	}
+	
+	@GetMapping("/getdailyworkout")
+	public String getDailyWorkOut() {
+		return myCoach.getDailyWorkOut();
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
